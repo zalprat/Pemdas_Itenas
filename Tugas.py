ea910880a2310c5ec8ff3295dceabbcd78f881e8
RIZAL <ASUS@LAPTOP-BM6H1N7U.(none)>
2023-12-09 19:09:30 +0700

    revisi 1

--- Tugas.py
+++ Tugas.py
@@ -7,20 +7,27 @@ data = {'Nama': ['John', 'Jane', 'Bob', 'Alice'],
 df = pd.DataFrame(data)
 
 # Pertanyaan 1:
-
 # Gunakan loop for dan fungsi lambda untuk menghitung gaji setiap karyawan setelah diberikan peningkatan sebesar 5% dari gaji saat ini.
+for index, row in df.iterrows():
+    df.at[index, 'Gaji'] = (lambda x: x * 1.05)(row['Gaji'])
 
 # Pertanyaan 2:
-
 # Setelah perubahan dilakukan, tampilkan DataFrame yang sudah diperbarui dan berikan ringkasan perubahan yang telah terjadi.
+print("DataFrame setelah peningkatan gaji 5%:")
+print(df)
 
 # Pertanyaan 3:
-
-# Gunakan loop for lagi untuk mengevaluasi karyawan yang usianya di atas 30 tahun. Jika usia karyawan di atas 30, berikan peningkatan tambahan sebesar 2% dari gaji saat ini menggunakan fungsi lambda.
+# Gunakan loop for lagi untuk mengevaluasi karyawan yang usianya di atas 30 tahun.
+# Jika usia karyawan di atas 30, berikan peningkatan tambahan sebesar 2% dari gaji saat ini menggunakan fungsi lambda.
+for index, row in df.iterrows():
+    if row['Usia'] > 30:
+        df.at[index, 'Gaji'] = (lambda x: x * 1.02)(row['Gaji'])
 
 # Pertanyaan 4:
-
 # Tampilkan DataFrame yang sudah diperbarui setelah peningkatan gaji tambahan dan berikan ringkasan hasilnya.
+print("\nDataFrame setelah peningkatan gaji tambahan:")
+print(df)
+
 
 # ---------------------------- #
 # Buat Branch Baru pada repository github berikut dengan format KELAS_NRP_NAMA
